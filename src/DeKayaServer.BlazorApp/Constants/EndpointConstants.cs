0c55c23253105cc0861b9236f2281080460b3cfc
Erdem Kaya <erdem.kaya@utb.ecutbildning.se>
2026-01-28 20:47:54 +0100

    role page v1.0

--- src/DeKayaServer.BlazorApp/Constants/EndpointConstants.cs
+++ src/DeKayaServer.BlazorApp/Constants/EndpointConstants.cs
@@ -4,8 +4,14 @@ public static class EndpointConstants
 {
     public const string Dashboard = "/";
     public const string LoginPage = "/login";
+
+    //Auth
     public const string LoginEndpoint = "auth/login";
     public const string ForgotPasswordEndpoint = "auth/forgotpassword";
     public const string ResetPasswordEndpoint = "auth/reset-password";
     public const string CheckForgotPasswordCodeEndpoint = "auth/check-forgot-password-code";
+
+    //Roles
+    public const string Roles = "/roles";
+    public const string ODataRoles = "odata/roles";
 }
