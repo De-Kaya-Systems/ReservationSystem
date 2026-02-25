bca2f4fb190d5053dbff93d065a09dd5b221d737
Erdem Kaya <erdem.kaya@utb.ecutbildning.se>
2026-02-25 21:16:12 +0100

    I created customer table

--- src/DeKayaServer.Infrastructure/Migrations/20260225201208_i_created_customer_table.Designer.cs
+++ src/DeKayaServer.Infrastructure/Migrations/20260225201208_i_created_customer_table.Designer.cs
@@ -4,6 +4,7 @@ using DeKayaServer.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DeKayaServer.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260225201208_i_created_customer_table")]
+    partial class i_created_customer_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/DeKayaServer.Infrastructure/Migrations/20260225201208_i_created_customer_table.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeKayaServer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class i_created_customer_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName_Value = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    LastName_Value = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    FullName_Value = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    Address_City = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    Address_District = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    Address_FullAddress = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    Contact_PhoneNumber = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    Contact_PhoneNumber2 = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    Contact_Email = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    UpdatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    DeletedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
