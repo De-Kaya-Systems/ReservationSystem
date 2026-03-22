27abb0042570d8305d43b8d7252daf9beb141638
Erdem Kaya <erdem.kaya@utb.ecutbildning.se>
2026-03-22 10:54:30 +0100

    has added UpdateCoolingRoomDialog

---
+++ src/DeKayaServer.Contracts/CoolingRooms/CoolingRoomMaintenanceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DeKayaServer.Contracts.CoolingRooms;
+
+public sealed class CoolingRoomMaintenanceDto
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; } = default!;
+    public DateOnly MaintenanceDateStart { get; set; }
+    public DateOnly MaintenanceDateEnd { get; set; }
+    public Guid StatusId { get; set; }
+}
---
+++ src/DeKayaServer.Contracts/CoolingRooms/CoolingRoomMaintenanceUpdateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace DeKayaServer.Contracts.CoolingRooms;
+
+public sealed class CoolingRoomMaintenanceUpdateRequest
+{
+    public bool Remove { get; init; }
+    public string? Description { get; init; }
+    public DateTime MaintenanceDateStart { get; init; }
+    public DateTime MaintenanceDateEnd { get; init; }
+}
