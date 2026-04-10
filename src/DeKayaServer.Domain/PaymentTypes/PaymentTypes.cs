ce60cd704bc4f61ca3353dccfb13a87843922a56
Erdem Kaya <erdem.kaya@utb.ecutbildning.se>
2026-04-10 14:43:37 +0200

    Added payment types table

---
+++ src/DeKayaServer.Domain/PaymentTypes/PaymentTypes.cs
@@ -0,0 +1,26 @@
+﻿using DeKayaServer.Domain.Abstractions;
+
+namespace DeKayaServer.Domain.PaymentTypes;
+
+public class PaymentTypes : Entity
+{
+    public PaymentTypes() { }
+
+    public PaymentTypes(
+        PaymentType paymentType,
+        bool isActive )
+    {
+        SetPaymentType( paymentType );
+        SetStatus( isActive );
+    }
+
+    public PaymentType PaymentType { get; private set; } = default!;
+
+    #region Behaviors
+    public void SetPaymentType( PaymentType paymentType )
+    {
+        PaymentType = paymentType;
+    }
+    #endregion
+}
+public sealed record PaymentType( string Value );
